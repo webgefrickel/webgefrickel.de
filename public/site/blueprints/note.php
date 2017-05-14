43834eac6ac45c49eb104087d3c2a6098c899c15
Steffen Rademacker <kontakt@webgefrickel.de>
2017-05-14 14:42:08 +0200

    update syndaction target for blueprint

--- public/site/blueprints/note.php
+++ public/site/blueprints/note.php
@@ -26,14 +26,12 @@ fields:
     label: In reply to
     type: url
     width: 1/2
-  twitter:
-    label: Tweet it
-    type: checkbox
-    text: Check to syndicate to Twitter on save
-    width: 1/2
   tags:
     label: Tags
     type: tags
   location:
     label: Geolocation
+    type: text
+  syndicate:
+    label: Syndication Target
     type: text
