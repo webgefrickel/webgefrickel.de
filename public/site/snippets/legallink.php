369a9d7f5671547c2953b1ba78010d7e4ad81f8f
Steffen Rademacker <kontakt@webgefrickel.de>
2018-05-23 14:35:02 +0200

    Minor fixes for new privacy policy in nav/legallink snippets

--- public/site/snippets/legallink.php
+++ public/site/snippets/legallink.php
@@ -2,3 +2,7 @@
 <a href="<?= $legal->url() ?>" class="link-legal  header__legal">
   <?= $legal->title() ?>
 </a>
+<?php $privacy = $pages->find('privacy-policy') ?>
+<a href="<?= $privacy->url() ?>" class="link-legal  header__legal">
+  <?= $privacy->title() ?>
+</a>
