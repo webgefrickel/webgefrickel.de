87e45793bc519b5308ce862765bc426ca745468e
Steffen Rademacker <kontakt@webgefrickel.de>
2019-07-10 14:38:05 +0200

    Add compiled output / new styleguide build

---
+++ public/site/controllers/home.php
@@ -0,0 +1,19 @@
+<?php
+
+return function($page) {
+  $data = kalong();
+  $tilePages = kirby()->site()->find('contact', 'work', 'blog');
+
+  $data['welcome'] = $page->welcome()->kirbytext();
+
+  foreach ($tilePages as $tilePage) {
+    $data['tiles'][] = [
+      'href' => $tilePage->url(),
+      'slug' => $tilePage->slug(),
+      'title' => $tilePage->title(),
+      'text' => $tilePage->introtext()->kirbytext(),
+    ];
+  }
+
+  return $data;
+};
